e6d6a310adcee872dbb03aaa158e5a80657b7723
ahmedsuliman62 <104222635+ahmedsuliman62@users.noreply.github.com>
2023-11-05 23:07:12 -0500

    Fixed the error in the program

--- SimpleWordProblem/SimpleWordProblem.java
+++ SimpleWordProblem/SimpleWordProblem.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class SimpleWordProblem {
+
+    public static boolean compare(int num1, int num2) {
+
+        if (num1 == num2) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner();
+        Scanner scan = new Scanner(System.in);
         Random rand = new Random();
-        rand1 = rand.nextInt(10) + 1;
-        rand2 = rand.nextInt(10) + 1;
+        int rand1 = rand.nextInt(10) + 1;
+        int rand2 = rand.nextInt(10) + 1;
 
         int sum = rand1 + rand2;
         System.out.println("If little Timmy has " + rand1 + " donuts, and Lily gives him "
@@ -15,7 +24,7 @@ public class SimpleWordProblem {
 
         int ans = scan.nextInt();
 
-        if (ans == sum) {
+        if (compare(ans, sum)) {
             System.out.println("That's the correct answer good job!");
         }
         else {
